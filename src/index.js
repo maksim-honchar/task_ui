32b53a77264e04bf4b68b8dbf931e7046a6d4ffd
Maksym Honchar <maksim.honchar@gmail.com>
2021-04-14 12:17:40 +0300

    added nav menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello Project')
+const ul = document.getElementById('topnav')
+const liElements = ul.getElementsByClassName('li')
+
+for (var i = 0; i < liElements.length; i++) {
+  liElements[i].addEventListener('click', function () {
+    const current = document.getElementsByClassName('active')
+    current[0].className = current[0].className.replace(' active', '')
+    this.className += ' active'
+  })
+}
